f57e9673ad604347619c28e494faec420d2f2a70
Nico Antonelli <niconelli2@gmail.com>
2019-09-13 15:11:33 -0300

    TP Academia - Lab03 - Sections 01-02-03 of 04
    Added Data.Database Project (Simulated Data Layer)
    Added BusinessLogic and UsuarioLogic Classes
    Added References
    Added FindAll, GetOne, Delete and Save Methods

---
+++ Academia/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic {
+    public class UsuarioLogic : BusinessLogic {
+
+        public UsuarioLogic(UsuarioAdapter UsuarioData) {
+            this.UsuarioData = UsuarioData;
+        }
+
+        // UsuarioAdapter
+        private Data.Database.UsuarioAdapter _UsuarioData;
+
+        public UsuarioAdapter UsuarioData { get; set; }
+
+        public List<Usuario> GetAll() {
+            return UsuarioData.GetAll();
+        }
+
+        public Business.Entities.Usuario GetOne(int ID) {
+            return UsuarioData.GetOne(ID);
+        }
+
+        public void Delete(int ID) {
+            UsuarioData.Delete(ID);
+        }
+
+        public void Save(Usuario usuario) {
+            UsuarioData.Save(usuario);
+        }
+    }
+}
